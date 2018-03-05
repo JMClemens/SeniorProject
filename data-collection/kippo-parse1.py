e5670d45876cf0df8564b40309ab13f6acc63134
JMClemens <joshuamclemens@gmail.com>
2018-03-05 14:34:31 -0500

    Currently working: IP addr, login attempts (username/password), and duration of session

--- data-collection/kippo-parse1.py
+++ data-collection/kippo-parse1.py
@@ -1,19 +1,46 @@
 import re
 
+def get_sec(time_str):
+    h, m, s = time_str.split(':')
+    return int(h) * 3600 + int(m) * 60 + int(s)
+
 sessionList = []
 x = 0
 log = open(r"kippo.log", "r").read()
 
-conn_list = re.split("New connection:.*",log)
+conn_list = re.split("(New connection:.*)",log)
 
 for session in conn_list:
-	x = x + 1
-	print "----------- New Session --------"
+	
+	print "-------- Session Start ---------"
 	ip =  re.findall(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', session)
-	set(ip)
-	for i in ip:
-		print "Ip item: " + i
-	la = re.findall("login attempt.*",session);
-	for bl in ip:
-		for line in la:
-			print "Run num: " + str(x) + " \nIP addr: " + bl + "  \nLogin Attempts: " + line
+	if ip:
+		ip = ip.pop(0)
+	else:
+		ip = 'none'
+	
+	la = re.findall("login attempt.*", session)
+	for line in la:
+		print "IP addr: " + ip  + "  \nLogin Attempts: " + line
+	
+	times = re.findall(r'[0-60]{2}\:[0-9][0-9]\:[0-9][0-9]\:*', session)
+	if len(times) > 1:
+		startTime = times.pop(0)
+		endTime = times.pop(len(times)-1)
+		duration = get_sec(str(endTime)) - get_sec(str(startTime))
+		m, s = divmod(duration, 60)
+		h, m = divmod(m, 60)
+		duration = "%02d:%02d:%02d" % (h, m, s)
+		#duration = str(h) + ":" + str(m) + ":" + str(s)
+	else:	
+		startTime = 0
+		endTime = 0
+		duration = 0
+
+	print "Start time: " + str(startTime)
+	print "End time: " +  str(endTime)
+	print "Duration: " + str(duration)
+
+	
+
+	print "-------- Session End ---------"
