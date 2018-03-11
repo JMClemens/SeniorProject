4ce5b66c1341304b16f1553992c2465c5dd1a99e
JMClemens <jmc28@live.hood.edu>
2018-03-11 16:51:26 -0700

    Testing pulling all country codes to make a csv file of them

---
+++ data-collection/test.py
@@ -0,0 +1,28 @@
+import pycountry
+import getCenterCords as g
+import time
+
+countryList = []
+
+def write_csv():
+	out_file = open("coords.csv","wb")
+	fieldnames = sorted(list(set(k for d in countryList for k in d)))
+	writer = csv.DictWriter(out_file, fieldnames=fieldnames, dialect='excel')
+
+	writer.writeheader()
+	for row in countryList:
+	    writer.writerow(row)
+	out_file.close()
+
+for item in pycountry.countries:
+	if(item.name == "Hong Kong"): codeCheck = "China"
+	elif(item.name == "Korea, Republic of"): codeCheck = "Korea"
+	else: codeCheck = item.name
+	coords = g.get_boundingbox_country(country=codeCheck, output_as='center')
+	time.sleep(1)
+
+	item = {"Country":codeCheck,"Coords":coords}
+
+	countryList.append(item)
+
+write_csv()
