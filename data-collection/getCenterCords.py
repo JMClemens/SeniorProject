2286ce6b0133c8c82189d49ed0f7febd27f1a5d3
JMClemens <jmc28@live.hood.edu>
2018-03-11 16:04:38 -0700

    Updated kippo parse to add coordinates - not working currently

---
+++ data-collection/getCenterCords.py
@@ -0,0 +1,34 @@
+import requests
+
+def get_boundingbox_country(country, output_as='boundingbox'):
+    """
+    get the bounding box of a country in EPSG4326 given a country name
+
+    Parameters
+    ----------
+    country : str
+        name of the country in english and lowercase
+    output_as : 'str
+        chose from 'boundingbox' or 'center'. 
+         - 'boundingbox' for [latmin, latmax, lonmin, lonmax]
+         - 'center' for [latcenter, loncenter]
+
+    Returns
+    -------
+    output : list
+        list with coordinates as str
+    """
+    # create url
+    url = '{0}{1}{2}'.format('http://nominatim.openstreetmap.org/search?country=',
+                             country,
+                             '&format=json&polygon=0')
+    response = requests.get(url).json()[0]
+
+    # parse response to list
+    if output_as == 'boundingbox':
+        lst = response[output_as]
+        output = [float(i) for i in lst]
+    if output_as == 'center':
+        lst = [response.get(key) for key in ['lat','lon']]
+        output = [float(i) for i in lst]
+    return output
