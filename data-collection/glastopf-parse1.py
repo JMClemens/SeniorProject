8441cc24a074876e25c91ae551e30626c0927a7a
JMClemens <jmc28@live.hood.edu>
2018-03-11 19:27:47 -0700

    Updated glastopf parsing to write a country frequency CSV files. New directory created for glastopf parsing files.

--- data-collection/glastopf-parse1.py
+++ data-collection/glastopf-parse1.py
@@ -1,22 +1,31 @@
 import pygeoip
 import getCenterCords as g
 import csv
+from collections import Counter
 
 fileName = "glastopf.log"
-outFile = "glastopf.csv"
+outFile = "glogs/glastopf.csv"
+gCfreqFile = "glogs/gcf.csv"
 gip = pygeoip.GeoIP("GeoIP.dat", pygeoip.MEMORY_CACHE)
 
 activityList = []
 
-def write_csv(fileName):
-	out_file = open(fileName,"wb")
-	fieldnames = sorted(list(set(k for d in activityList for k in d)))
-	writer = csv.DictWriter(out_file, fieldnames=fieldnames, dialect='excel')
+def write_list_of_dicts_to_csv(fileName, list_of_dicts):
+	with open(fileName,"wb") as out_file:
+		fieldnames = sorted(list(set(k for d in list_of_dicts for k in d)))
+		writer = csv.DictWriter(out_file, fieldnames=fieldnames, dialect='excel')
+		writer.writeheader()
+		for row in list_of_dicts:
+		    writer.writerow(row)
 
-	writer.writeheader()
-	for row in activityList:
-	    writer.writerow(row)
-	out_file.close()
+def write_dict_to_csv(fileName,fieldNames, myDict):
+	with open(fileName,"wb") as out_file:
+		fieldnames = fieldNames
+		writer = csv.DictWriter(out_file, fieldnames=fieldnames, dialect='excel')
+		writer.writer.writerow
+		for key, value in myDict.items():
+			writer.writerow([key, value])
+
 
 def parseLog(fileName):
 	with open(fileName, "r") as file:
@@ -36,10 +45,21 @@ def parseLog(fileName):
 
 			#add coords back to dict when possible
 			entry = {"Date":date, "Timestamp":timeStamp, "IP":ipAddr, "Country":countryName, "StatusCode":httpStatusCode,"RequestMethod":httpRequestMethod,"Resource":requestedResource}
-			print entry
+			#print entry
 			activityList.append(entry)
 
-
+def countryFrequency():
+	countryList = []
+	for item in activityList:
+		countryList.append(item["Country"])
+	countryFrequency = Counter(countryList)
+	countryFrequency = dict(countryFrequency)
+	newCountryList = []
+	for key, value in countryFrequency.items():
+		entry = {"Country":key,"Frequency":value}
+		newCountryList.append(entry)
+	write_list_of_dicts_to_csv(gCfreqFile,newCountryList)	
 
 parseLog(fileName)
-write_csv(outFile)
+write_list_of_dicts_to_csv(outFile,activityList)
+countryFrequency()
