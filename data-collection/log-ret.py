e4b4980c929a97345c6bc31e3b07cf2d80075f47
JMClemens <joshuamclemens@gmail.com>
2018-03-28 13:19:41 -0400

    Fixing incorrect path name

--- data-collection/log-ret.py
+++ data-collection/log-ret.py
@@ -3,7 +3,7 @@ import os
 import subprocess
 
 glastopfLogPath = '../jmc/glastopf/financialfirstgroup/log/'
-glastopfLogDestinationPath = "~/home/jmc/glogs"
+glastopfLogDestinationPath = "~/home/jmc/glogs/"
 glastopfLogs = []
 
 def getAllGlastopfLogs():
