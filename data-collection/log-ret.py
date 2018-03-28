a74d7098739b60271c6fc9b0d3c5fe116d94212b
JMClemens <joshuamclemens@gmail.com>
2018-03-28 13:08:14 -0400

    Renaming todays log fixing

--- data-collection/log-ret.py
+++ data-collection/log-ret.py
@@ -3,13 +3,20 @@ import os
 import subprocess
 
 glastopfLogPath = '../jmc/glastopf/financialfirstgroup/log/'
+glastopfLogDestinationPath = "glogs"
 glastopfLogs = []
 
 def getAllGlastopfLogs():
+
+	# Get all files in the glastopf log directory 
 	logs = [f for f in os.listdir(glastopfLogPath) if os.path.isfile(os.path.join(glastopfLogPath, f))]
+	
+	# Copy each file over
 	for log in logs:
-		if log == "glastopf.log":
-			log = log + str(datetime.date.today())
-		subprocess.Popen(["scp", glastopfLogPath+log, "glogs"]).wait()
+		subprocess.Popen(["scp", glastopfLogPath+log, glastopfLogDestinationPath]).wait()
+
+	# Rename current day's log to be timestamped
+	os.chdir(glastopfLogDestinationPath)
+	subprocess.Popen(["scp", glastopfLogPath+"glastopf.log", glastopfLogDestinationPath+"glastopf.log."+datetime.date.today()]).wait()
 
 getAllGlastopfLogs()
