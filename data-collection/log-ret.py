29285b6bef70035be5c399e9d3528f491ca4ce83
JMClemens <joshuamclemens@gmail.com>
2018-03-28 13:02:11 -0400

    Renaming current log to today's date

--- data-collection/log-ret.py
+++ data-collection/log-ret.py
@@ -8,6 +8,8 @@ glastopfLogs = []
 def getAllGlastopfLogs():
 	logs = [f for f in os.listdir(glastopfLogPath) if os.path.isfile(os.path.join(glastopfLogPath, f))]
 	for log in logs:
+		if log = "glastopf.log":
+			log = log + str(datetime.date.today())
 		subprocess.Popen(["scp", glastopfLogPath+log, "glogs"]).wait()
 
 getAllGlastopfLogs()
