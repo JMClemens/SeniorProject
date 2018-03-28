a8f7bf2ead97b4fd730e3fcb20353e59883bcbe5
Cory Watson <corywatson@Corys-MacBook-Pro.local>
2018-03-28 15:19:20 -0400

    Test file for time stamping kippo logs

---
+++ data-collection/file-ret-kippo.py
@@ -0,0 +1,66 @@
+from paramiko import SSHClient
+from scp import SCPClient
+import paramiko as pm
+import datetime
+import os
+
+
+server = "192.250.236.104"
+port = "1026"
+user = "datagrabber"
+password = "HcHp18&cj"
+
+
+kippoStartPath = '../caw/kippo/kippo/log/'
+kippoLogFile = 'kippo.log'
+kippoLogs = []
+
+kippoFilePath = '../caw/kippo/kippo/log/kippo.log'
+
+def createSSHClient(server, port, user, password):
+	client = SSHClient()
+	client.load_system_host_keys()
+	client.set_missing_host_key_policy(pm.AutoAddPolicy())
+	client.connect(server, port, user, password)
+	return client
+
+
+def generateAllKLogPaths():
+	kippoLogs.append(kippoStartPath)
+	startDate = "2018-02-26"
+	start = datetime.datetime.strptime(startDate, '%Y-%m-%d')
+	today = datetime.date.today()
+	end  = datetime.datetime(*(today.timetuple()[:3]))
+	step = datetime.timedelta(days=1)
+	while start < end:
+		newPath = kippoStartPath + '.' + start.strptime ('%Y-%m-%d')
+ 		logPath = kippoStartPath+kippoLogFile
+		kippoLogs.append(logPath)
+		start += step
+
+
+	# TODO:
+	# need a better way to figure out how many logs there are
+	# also need to date these logs once they're processed
+	
+	for i in range(1,10):
+		newPath = logPath + '.' + str(i)
+		print newPath
+		kippoLogs.append(newPath)
+
+
+def getAllKippoLogs():
+	generateAllKLogPaths()
+	for log in kippoLogs:
+		os.chdir('kippo/logs')
+		scp.get(log)
+		os.chdir('../../')
+
+ssh = createSSHClient(server, port, user, password)
+scp = SCPClient(ssh.get_transport())
+
+
+# Gets the logs from kippo
+getAllKippoLogs()
+
+scp.close()
