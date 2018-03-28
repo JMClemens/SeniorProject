303957a35b0fb0be6e6ceb51dd9f4deff28f636b
JMClemens <joshuamclemens@gmail.com>
2018-03-28 13:03:30 -0400

    Equality error fixing

--- data-collection/log-ret.py
+++ data-collection/log-ret.py
@@ -8,7 +8,7 @@ glastopfLogs = []
 def getAllGlastopfLogs():
 	logs = [f for f in os.listdir(glastopfLogPath) if os.path.isfile(os.path.join(glastopfLogPath, f))]
 	for log in logs:
-		if log = "glastopf.log":
+		if log == "glastopf.log":
 			log = log + str(datetime.date.today())
 		subprocess.Popen(["scp", glastopfLogPath+log, "glogs"]).wait()
 
