073f6ea585736ec1e6d6670101c3ad70a44ed205
Joshua Clemens <joshuamclemens@gmail.com>
2018-04-08 18:05:18 -0400

    Working on pie chart - messing up other graph currently

--- financialfirstgroup.com/assets/js/graphs/glastopf-requests.js
+++ financialfirstgroup.com/assets/js/graphs/glastopf-requests.js
@@ -1,46 +1,44 @@
-var width = 450,
-		height = 450,
+var width = 300,
+		height = 300,
 		radius = Math.min(width, height) / 2;
 		
-var color = ["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c"];
+var dataset = [];
+
+var color = d3.scale.category20();
+
+var svg = d3.select('#pieChart')
+	.append('svg')
+	.attr('width', width)
+	.attr('height', height)
+	.append('g')
+	.attr('transform', 'translate(' + (width / 2) +
+		',' + (height / 2) + ')');
+		
+var arc = d3.svg.arc()
+	.innerRadius(0)
+	.outerRadius(radius);
+
+var pie = d3.layout.pie()
+	.value(function(d) { return d.count; })
+	.sort(null);
 	
-d3.csv("grf.csv", function(error, data) {
+d3.csv("../assets/data/grf.csv", function(error, data) {
 
   data.forEach(function(d) {
-    d.Frequency = +d.Frequency;
+    dataset.push({
+			label: d.Request,
+			count: +d.Frequency
+		})
   });
 	
-var svg = d3.select("g-request-pie").append("svg")
-		.data(data)
-    .attr("width", width)
-    .attr("height", height)
-    .append("g")
-    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-		
-var pie = d3.layout.pie().value(function(d){return d.Frequency;});
-
-// Declare an arc generator function
-var arc = d3.svg.arc().outerRadius(radius);
-
-// Select paths, use arc generator to draw
-var arcs = svg.selectAll("g.slice")
-		.data(pie)
-		.enter()
-		.append("g")
-		.attr("class", "slice");
-arcs.append("path")
-    .attr("fill", function(d, i){return color[i];})
-    .attr("d", function (d) {return arc(d);});
-		
-// Add text
-arcs.append("text")
-    .attr("transform", function(d){
-        d.innerRadius = 100; /* Distance of label to the center*/
-        d.outerRadius = r;
-        return "translate(" + arc.centroid(d) + ")";}
-    )
-    .attr("text-anchor", "middle")
-    .text( function(d) {return d.Frequency;});
-
-
+	var path = svg.selectAll('path')
+	.data(pie(dataset))
+	.enter()
+	.append('path')
+	.attr('d', arc)
+	.attr('fill', function(d) {
+		return color(d.data.label);
+	});
 	
+});
+
