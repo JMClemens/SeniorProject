c3ef79223a942a9192c1b25973adca07b6d8e3c1
Cory Watson <corywatson@Corys-MacBook-Pro.local>
2018-04-09 23:57:36 -0400

    Updated Sections

--- financialfirstgroup.com/assets/js/graphs/kippo-graph-2.js
+++ financialfirstgroup.com/assets/js/graphs/kippo-graph-2.js
@@ -1,49 +1,57 @@
-// Define the height/width of our svg and its margins
-var margin = { top: 20, right: 10, bottom: 100, left: 40},
-    width = 500 - margin.right - margin.left,
-    height = 350 - margin.top - margin.bottom;
 
-// Define svg    
-var svg = d3.select("#Duration")
-    .append("svg")
-      .attr ({
-        "width": width + margin.right + margin.left,
-        "height": height + margin.top + margin.bottom
-      })
-    .append("g")
-      .attr("transform","translate(" + margin.left + "," + margin.right + ")");
+var color = "blue";
+// Define the height/width of our svg and its margins
+var values = d3.range(1000).map(d3.random.normal(20, 5));
+
+// A formatter for counts.
+var formatCount = d3.format(",.0f");
+
+var margin = {top: 20, right: 30, bottom: 30, left: 30},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
 // Define x and y scales
-var xScale = d3.scale.ordinal()
-    .rangeRoundBands([0,width], 0.2, 0.2);
+var max = d3.max(values);
+var min = d3.min(values);
+var x = d3.scale.linear()
+      .domain([min, max])
+      .range([0, width]);
 
-var yScale = d3.scale.linear()
-    .rangeRound([height,0]);
+// Generate a histogram using twenty uniformly-spaced bins.
+var data = d3.layout.histogram()
+    .bins(x.ticks(20))
+    (values);
+
+var yMax = d3.max(data, function(d){return d.length});
+var yMin = d3.min(data, function(d){return d.length});
+var colorScale = d3.scale.linear()
+            .domain([yMin, yMax])
+            .range([d3.rgb(color).brighter(), d3.rgb(color).darker()]);
+
 
 // Define axis
 var xAxis = d3.svg.axis()
     .scale(xScale)
     .orient("bottom");
+
+    // Define svg    
+var dvg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
     
-var yAxis = d3.svg.axis()
-    .scale(yScale)
-    .orient("left");
+var y = d3.scale.linear()
+    .domain([0, yMax])
+    .range([height, 0]);
 
 // Import Glastopf country frequency CSV file    
 d3.csv("../assets/data/kdur.csv", function(error, data) {
     
     if(error) console.log("Error: data not loaded");
     
-    data.forEach(function(d) {      
-      // + symbol convert from string representation of a number to an actual number
-      d.Frequency = +d.Frequency;
-      d.Country = d.Country;
-    });
-    
-    // sort the frequency values
-    data.sort(function(a,b) {
-       return b.Frequency - a.Frequency
-    });
+
     
     // specify domains of x and y scales
     xScale.domain(data.map(function(d) { return d.Country }) );
@@ -52,7 +60,7 @@ d3.csv("../assets/data/kdur.csv", function(error, data) {
     yAxis.ticks( Math.min(10, (yDomain[1] - yDomain[0]) ) );
     
     // draw bars
-    svg.selectAll('rect')
+    dvg.selectAll('rect')
       .data(data)
       .enter()
       .append('rect')
@@ -63,40 +71,32 @@ d3.csv("../assets/data/kdur.csv", function(error, data) {
       .transition().duration(2000)
       .delay(function(d,i) { return i * 150;})
       .attr ({
-        "x": function(d) { return xScale(d.Country); },
-        "y": function(d) { return yScale(d.Frequency); },
-        "width": xScale.rangeBand(),
-        "height": function(d) {return height - yScale(d.Frequency);}
       })
       // Can use this notation below to fill a different color for each graph
       .style("fill", function(d,i) { return 'rgb(20, 20, ' + ((i * 30) + 100) + ')'});
       
       // label the bars
-      svg.selectAll('text')
-        .data(data)
-        .enter()
-        .append('text')
-        .text(function(d) { return d.Frequency; })
-        .attr('x', function(d) { return xScale(d.Country) + xScale.rangeBand()/2;})
-        .attr('y', function(d) { return yScale(d.Frequency) + 12;})
-        .style("fill", "white")
-        .style("text-anchor", "middle");
-      
-      // draw x axis
-      svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis)
-        .selectAll('text')
-        .attr("transform", "rotate(-60)")
-        .attr("dx","-.8em")
-        .attr("dy",".25em")
-        .style("text-anchor","end")
-        .style("font-size","12px");
-      
-      // draw y axis
-      svg.append("g")
-        .attr("class","y axis")
-        .call(yAxis)
-        .style("font-size","12px");
+  dvg.selectAll(".bar")
+    .data(data)
+  .enter().append("g")
+    .attr("class", "bar")
+    .attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; });
+
+bar.append("rect")
+    .attr("x", 1)
+    .attr("width", (x(data[0].dx) - x(0)) - 1)
+    .attr("height", function(d) { return height - y(d.y); })
+    .attr("fill", function(d) { return colorScale(d.y) });
+
+bar.append("text")
+    .attr("dy", ".75em")
+    .attr("y", -12)
+    .attr("x", (x(data[0].dx) - x(0)) / 2)
+    .attr("text-anchor", "middle")
+    .text(function(d) { return formatCount(d.y); });
+
+dvg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
 });
